3f7e6bc1ea741124a35939c237993bda0bc02c7f
Said Alimov <120774372+kindevelopment@users.noreply.github.com>
2024-04-17 16:52:29 +0300

    Added static for media app (#13)

--- src/app/urls.py
+++ src/app/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -15,3 +17,6 @@ urlpatterns = [
 
 urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
 urlpatterns += spectacular_urls
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
