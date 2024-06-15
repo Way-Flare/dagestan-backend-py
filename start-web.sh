d1a17c3e684b27d22cbb7f2873020b388a960709
etozhefifan <coofi2012@gmail.com>
2024-06-15 11:58:32 +0300

    fix script

--- start-web.sh
+++ start-web.sh
@@ -5,8 +5,9 @@ set -euo pipefail
 python manage.py collectstatic --no-input
 python manage.py migrate
 if [[ "$DJANGO_ENV" = "PRODUCTION" ]]; then
+  DJANGO_LOG_LEVEL_LOWER=$(echo "$DJANGO_LOG_LEVEL" | tr '[:upper:]' '[:lower:]')
   gunicorn -b 0.0.0.0:8000 \
-    --log-level ${DJANGO_LOG_LEVEL,,} \
+    --log-level $DJANGO_LOG_LEVEL_LOWER \
     --access-logfile - app.wsgi:application
 else
   python manage.py runserver 0.0.0.0:8000
