a65c2b7c5128175bb7cc1e448fead208cb3514c2
said alimov <saidalimov.1998@mail.ru>
2024-06-15 12:50:33 +0300

    Added start sh file for running web app django to gunicorn

---
+++ start-web.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euo pipefail
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+if [[ "$DJANGO_ENV" = "PRODUCTION" ]]; then
+  gunicorn -b 0.0.0.0:8000 \
+    --log-level ${DJANGO_LOG_LEVEL,,} \
+    --access-logfile - app.wsgi:application
+else
+  python manage.py runserver 0.0.0.0:8000
+fi
