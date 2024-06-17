b5de16924fc52dc98afd783292c1e31f3e88df11
etozhefifan <94104800+etozhefifan@users.noreply.github.com>
2024-06-17 20:03:20 +0300

    h/logfix

--- start-web.sh
+++ start-web.sh
@@ -6,9 +6,7 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 if [[ "$DJANGO_ENV" = "PRODUCTION" ]]; then
   DJANGO_LOG_LEVEL_LOWER=$(echo "$DJANGO_LOG_LEVEL" | tr '[:upper:]' '[:lower:]')
-  gunicorn -b 0.0.0.0:8000 \
-    --log-level $DJANGO_LOG_LEVEL_LOWER \
-    --access-logfile - app.wsgi:application
+  gunicorn -b 0.0.0.0:8000 app.wsgi:application
 else
   python manage.py runserver 0.0.0.0:8000
 fi
