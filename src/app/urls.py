25aec524003eb745b56efb5dfb8b99f41590e304
etozhefifan <coofi2012@gmail.com>
2024-06-17 08:00:14 +0300

    added staticfiles to urls

--- src/app/urls.py
+++ src/app/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from app.spectacular import urlpatterns as spectacular_urls
 
@@ -13,5 +14,6 @@ urlpatterns = [
     path('v1/', include(v1)),
 ]
 
+urlpatterns += staticfiles_urlpatterns()
 urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
 urlpatterns += spectacular_urls
